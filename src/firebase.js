9b043c1aae51003e0da3eb99b0ca08a812393a14
manish <manishkr9547@gmail.com>
2025-03-22 19:18:40 +0530

    add Firebase authentication; implement login/signup functionality; enhance Navbar and Login styles; update Home and Footer styles for responsiveness

---
+++ src/firebase.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6BTf5FOy41PCgSKpZwr9r2h7qTqomuMU",
+  authDomain: "netflix-df513.firebaseapp.com",
+  projectId: "netflix-df513",
+  storageBucket: "netflix-df513.firebasestorage.app",
+  messagingSenderId: "845990472470",
+  appId: "1:845990472470:web:254d89acd1981c881d3e37",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export { auth, db, login, signup, logout };
